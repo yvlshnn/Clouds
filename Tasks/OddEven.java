a1ad2f3b22a335992d46ce083e1217e54ffd3c6a
yvlshnn <loveytaguinod08@gmail.com>
2024-12-08 13:04:15 +0000

    Done

---
+++ Tasks/OddEven.java
@@ -0,0 +1,14 @@
+public class OddEven {
+    public static void main(String[] args) {
+        int check_number = 15;
+        String message;
+
+        for (int i = 1; i <= check_number; i++){
+            message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
+
+            System.out.println(message);
+        }
+        
+    }
+}
+
